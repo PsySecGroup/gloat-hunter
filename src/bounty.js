6325738c113c3ae5ce4bab6c0db1c691a1d6fb79
HyperCrowd <info@hypercrowd.org>
2023-06-01 23:33:43 -0400

    bounty

--- src/bounty.js
+++ src/bounty.js
@@ -1,42 +1,112 @@
 // https://github.com/feross/simple-peer
 
-let peers
+// Create a Keystore
+const keystore = {};
 
-/**
- * 
- */
+// Initialize Simple Peer Instances
+const peers = [];
+
 function loadBountySystem() {
   if (api === undefined) {
-    loadScript('https://cdnjs.cloudflare.com/ajax/libs/simple-peer/9.7.0/simplepeer.min.js', () => {
-      peers = new SimplePeer({
-        initiator: false,
-        trickle: false,
-        config: {
-          iceServers: [
-            { urls: 'stun:relay.webwormhole.io:3478' },
-            { urls: 'stun:stun.nextcloud.com:443' }
-          ]
-        },
-      })
-
-      // @TODO figure this out!
-      peers.on('error', console.error)
-
-      peers.on('signal', data => {
-        console.log('SIGNAL', data)
-        peers.signal(data)
-      })
-
-      peers.on('connect', () => {
-        console.log('CONNECT')
-        peers.send('whatever' + Math.random())
-      })
-
-      peers.on('data', data => {
-        console.log('data: ' + data)
-      })
-
-      //document.getElementById('loadingBounty').remove()
-    })
+    loadScript('https://cdnjs.cloudflare.com/ajax/libs/simple-peer/9.7.0/simplepeer.min.js', connectToPeers)
   }
 }
+
+// Establish Connections
+function connectToPeers() {
+  // Connect to signaling server and retrieve connection details
+  const discoveredPeers = discoverPeers(); // Implement this function
+
+  // Create a new SimplePeer instance for each peer in the network
+  discoveredPeers.forEach(peerData => {
+    const peer = new SimplePeer({
+      initiator: false,
+      trickle: false,
+      config: {
+        iceServers: [
+          { urls: 'stun:relay.webwormhole.io:3478' },
+          { urls: 'stun:stun.nextcloud.com:443' }
+        ]
+      }
+    });
+
+    // Handle connection events
+    peer.on('signal', data => {
+      // Send signaling data to the remote peer
+      sendSignalingData(peerData.id, data); // Implement this function
+    });
+
+    peer.on('connect', () => {
+      console.log('Connected to peer:', peerData.id);
+    });
+
+    peer.on('data', data => {
+      handleKeystoreUpdate(JSON.parse(data)); // Implement this function
+    });
+
+    peer.on('close', () => {
+      console.log('Connection closed:', peerData.id);
+    });
+
+    // Add the new peer to the peers array
+    peers.push(peer);
+  });
+}
+
+function discoverPeers() {
+  // Simulated list of discovered peer IDs
+  const discoveredPeers = ['peer1', 'peer2', 'peer3'];
+  
+  return discoveredPeers;
+}
+
+// Synchronize Keystore
+function synchronizeKeystore() {
+  // Broadcast the keystore update to all connected peers
+  const keystoreUpdate = JSON.stringify(keystore);
+
+  peers.forEach(peer => {
+    peer.send(keystoreUpdate);
+  });
+}
+
+function sendSignalingData(peerId, data) {
+  // Simulated direct signaling data exchange between peers
+  const peer = getPeerById(peerId); // Implement this function to get the SimplePeer instance by peerId
+  peer.signal(data);
+}
+
+// Handle Keystore Updates
+function handleKeystoreUpdate(updatedKeystore) {
+  // Merge the received keystore update with the local keystore
+  Object.assign(keystore, updatedKeystore);
+  console.log('Keystore synchronized:', keystore);
+}
+
+// Broadcast Keystore Updates
+function broadcastKeystoreUpdate() {
+  const keystoreUpdate = JSON.stringify(keystore);
+  peers.forEach(peer => {
+    peer.send(keystoreUpdate);
+  });
+}
+
+function updateKeystore(key, value) {
+  // Update the local keystore
+  keystore[key] = value;
+
+  // Create a keystore update message
+  const keystoreUpdate = JSON.stringify({ [key]: value });
+
+  // Broadcast the keystore update to all connected peers
+  peers.forEach(peer => {
+    peer.send(keystoreUpdate);
+  });
+}
+
+// Example Usage
+//connectToPeers();
+
+// Update the keystore
+//keystore['key1'] = 'value1';
+//broadcastKeystoreUpdate();
